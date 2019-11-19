067989a373c6c49f132fbdcf3ec76f1cd654b9f6
Jermaine217 <55493849+Jermaine217@users.noreply.github.com>
2019-11-19 11:48:25 +0200

    update

---
+++ Hangman/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace Hangman
+{
+    public class HangManConsole
+    {
+        public static void Main(string[] args)
+        {
+            PrintMessage("My Message");
+        }
+    
+        public static void PrintMessage(string message, bool printTop = true, bool printBottom = true)
+        {
+            if (printTop)
+            {
+                Console.WriteLine("+---------------------------------------+");
+                Console.WriteLine("|");
+            }
+            bool front = true;
+
+            for (int i = message.Length; i < 33; i++)
+            {
+                if (front)
+                {
+                    message = " " + message;
+                }
+                else
+                {
+                    message = message + " ";
+
+                }
+                front = !front;
+            }
+            Console.WriteLine(message);
+
+            if (printBottom)
+            {
+                Console.WriteLine("|"); // check out here
+                Console.WriteLine("+-----------------------------------------+");
+            }
+        }
+    }
+} 
